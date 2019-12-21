1098e04c3a348d6f5defd9300bf4b6607beed144
Aleksandr Yuryev <veyruyskela@gmail.com>
2019-12-21 19:17:16 +0400

    pdp styles

--- src/components/PDP/TakeItDetails/index.js
+++ src/components/PDP/TakeItDetails/index.js
@@ -21,23 +21,27 @@ class TakeItDetails extends Component{
 
         return(
             <div className={'donate-details'}>
-                <h3 className={'donate-details__header'}>Title{title}</h3>
-                <p className={'donate-details__city-date'}>Baku {city}, 12-04-2019 {date}</p>
-                <div className={'donate-details__desc-container'}>
-                    <p className={'donate-details__title'}>Description</p>
-                    <p>{description} Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aspernatur autem cupiditate, delectus dicta dignissimos earum, exercitationem iusto nesciunt numquam, officiis praesentium ratione repellat reprehenderit rerum saepe similique tempora tenetur.</p>
-                    <p  className={'donate-details__title'}>Category</p>
-                    <p>Books {category}</p>
+                <div className={'donate-details__top'}>
+                    <h3 className={'donate-details__header'}>Title{title}</h3>
+                    <p className={'donate-details__city-date'}>Baku {city}, 12-04-2019 {date}</p>
+                    <div className={'donate-details__desc-container takeIt'}>
+                        <p className={'donate-details__title'}>Description</p>
+                        <p>{description} Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aspernatur autem cupiditate, delectus dicta dignissimos earum, exercitationem iusto nesciunt numquam, officiis praesentium ratione repellat reprehenderit rerum saepe similique tempora tenetur.</p>
+                        <p  className={'donate-details__title'}>Category</p>
+                        <p>Books {category}</p>
+                    </div>
                 </div>
-                <div className={'donate-details__user-info'}>
-                    <p className={'donate-details__title'}>User Info</p>
-                    {userInfo.map((info,index)=>{
-                        return  <UserInfo
-                            key={index}
-                            title={info.title}
-                            content={info.content}
-                        />
-                    })}
+                <div className={'donate-details__bottom'}>
+                    <div className={'donate-details__user-info'}>
+                        <p className={'donate-details__title'}>User Info</p>
+                        {userInfo.map((info,index)=>{
+                            return  <UserInfo
+                                key={index}
+                                title={info.title}
+                                content={info.content}
+                            />
+                        })}
+                    </div>
                 </div>
             </div>
         )
