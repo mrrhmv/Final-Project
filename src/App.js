287f6c5ec51b1308d003254fb1a88adadaa2dd54
Aleksandr Yuryev <veyruyskela@gmail.com>
2019-12-21 12:00:35 +0400

    added menu

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
                 <Switch>
                     <Route path='/' exact>
                     <Header/>
-                    <Menu/>
+                    <Menu/>/*added*/
                     </Route>
                     <Route path='/new'>
                         <Header/>
