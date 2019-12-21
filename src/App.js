e9dea5824f416e095b88584fd7cbfd8ff4aefa7d
Aleksandr Yuryev <veyruyskela@gmail.com>
2019-12-21 12:15:10 +0400

    added menu import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './reset.scss'
 import Header from "./components/HomePage/Header";
+import Menu from "./components/HomePage/Menu";
 import CreateNewPost from "./components/CreateNewPost";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import PDP from "./components/PDP";
